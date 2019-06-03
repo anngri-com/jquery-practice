f50a45f76fb176e0b6365b67af61f8f199d7be50
Anna Gritsai and Na Hyung Choi <na@>
2019-06-03 13:15:54 -0700

    change jQuery functions to dollar signs and put in .ready function

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 
-jQuery("h1").click(function() {
-  alert("This is a header.");
-});
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
 
-jQuery("img").click(function() {
-  alert("This is an image.");
+  $("img").click(function() {
+    alert("This is an image.");
+  });
 });
